13fc69cadec0847b31136690217f030f157da9e9
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-08 16:50:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
 function writeCards(nameArray, message) {
   messageArray = []
-  for (let i = 0, i < nameArray.length, i++){
+  for (let i = 0; i < nameArray.length; i++){
     messageArray.push('Thank you, ${nameArray[i]}, for the wonderful ${message} gift!')
   }
 }
