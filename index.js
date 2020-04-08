87c449ae2e19049faf2fc995f4564669c1f2e66a
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-08 17:00:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(nameArray, message) {
+  let messageArray = [];
+  for (let i = 0; i < nameArray.length; i++) {
+    messageArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${message} gift!`);
+  }
+  return messageArray;
+}
+
+function countDown(inputNum) {
+  let num = inputNum
+  while (num >= 0) {
+    console.log(num--)
+  }
+}
