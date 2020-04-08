db82157c818ad43149a1cbe91b271c2b6ee9ad9c
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-08 16:48:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+function writeCards(nameArray, message) {
+  messageArray = []
+  for (let i = 0, i < nameArray.length, i++){
+    messageArray.push("Thank you, Ada, for the wonderful birthday gift!")
+  }
+}
